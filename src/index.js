d0b25be2a3eb4b82db1856ed46921a173fa969eb
Komal giri <komalgiri789@gmail.com>
2025-09-20 23:18:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import podRoutes from "./routes/podRoutes.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -18,4 +19,6 @@ app.get("/", (req, res) => {
   res.json({ message: "CodePods backend running ✅" });
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
